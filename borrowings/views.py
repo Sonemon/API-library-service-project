1bfcdae61b570ae53a9991f80a6efd6ae24b982b
MRGL <Sonemongoo@gmail.com>
2025-04-17 21:46:04 +0300

    Added list and detail view with permissions

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
 
-# Create your views here.
+
+class BorrowingListRetrieveView(generics.ListAPIView, generics.RetrieveAPIView):
+    serializer_class = BorrowingSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Borrowing.objects.all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        return queryset
