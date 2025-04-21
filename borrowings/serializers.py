dc09dce15a0894d1bf492d556fcb6d4729514615
MRGL <Sonemongoo@gmail.com>
2025-04-21 17:09:20 +0300

    Added create serializer for borrowing with validators

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from django.utils import timezone
+
 from borrowings.models import Borrowing
+from books.models import Book
 from books.serializers import BookSerializer
 from users.serializers import UserSerializer
 
@@ -18,3 +21,33 @@ class BorrowingSerializer(serializers.Serializer):
             "book",
             "user"
         ]
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "book",
+            "expected_return_date",
+            "user"
+        ]
+        extra_kwargs = {
+            "book": {"required": True},
+            "expected_return_date": {"required": True},
+        }
+
+        def validate_book(self, book: Book):
+            if book.inventory <= 0:
+                raise serializers.ValidationError("This book is out of stock.")
+            return book
+
+        def validate(self, attrs):
+            if attrs['expected_return_date'] <= timezone.now().date():
+                raise serializers.ValidationError({
+                    'expected_return_date': 'Return date must be in the future'
+                })
+            return attrs
