2efd4aa21faef5b34f7caafa9e221dd0e81c0a58
MRGL <Sonemongoo@gmail.com>
2025-04-22 00:37:06 +0300

    Added urls for all borrowing endpoints

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from borrowings.views import (
-    BorrowingListRetrieveView, BorrowingCreateView,
+    BorrowingListView,
+    BorrowingRetrieveView,
+    BorrowingCreateView,
+    BorrowingReturnView
 )
 
 app_name = "borrowings"
 
 urlpatterns = [
-    path("", BorrowingListRetrieveView.as_view(), name="borrowing_list_retrieve"),
-    path("create/", BorrowingCreateView.as_view(), name="borrowing_create")
+    path("", BorrowingListView.as_view(), name="borrowing_list"),
+    path("create/", BorrowingCreateView.as_view(), name="borrowing_create"),
+    path("<int:pk>/", BorrowingRetrieveView.as_view(), name="borrowing_detail"),
+    path("<int:pk>/return/", BorrowingReturnView.as_view(), name="borrowing_return"),
 ]
