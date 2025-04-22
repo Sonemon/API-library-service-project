30eabc848d8a8c93f9ce60c5932b90c0204881fe
MRGL <Sonemongoo@gmail.com>
2025-04-22 03:00:26 +0300

    Added better visual for notifications

--- telegram_notifications/message_builder.py
+++ telegram_notifications/message_builder.py
@@ -1,18 +1,18 @@
 def build_borrowing_created_message(borrowing):
     return (
-        f"📚 *New Borrowing Created*\n\n"
-        f"*ID:* {borrowing.id}\n"
-        f"*User:* {borrowing.user.email}\n"
-        f"*Book:* {borrowing.book.title}\n"
-        f"*Borrow Date:* {borrowing.borrow_date.strftime("%Y-%m-%d %H:%M")}\n"
-        f"*Expected Return:* {borrowing.expected_return_date}"
+        f"📚 *New Borrowing Created*\n"
+        f"ID: {borrowing.id}\n"
+        f"User: {borrowing.user.email} (id: {borrowing.user.id})\n"
+        f"Book: {borrowing.book.title} (id: {borrowing.book.id})\n"
+        f"Borrow Date: {borrowing.borrow_date.strftime("%Y-%m-%d %H:%M")}\n"
+        f"Expected Return: {borrowing.expected_return_date}"
     )
 
 def build_borrowing_closed_message(borrowing):
     return (
         f"✅ Borrowing Closed\n"
         f"ID: {borrowing.id}\n"
-        f"User: {borrowing.user.email}\n"
-        f"Book: {borrowing.book.title}\n"
+        f"User: {borrowing.user.email} (id: {borrowing.user.id})\n"
+        f"Book: {borrowing.book.title} (id: {borrowing.book.id})\n"
         f"Returned on: {borrowing.actual_return_date.strftime("%Y-%m-%d %H:%M")}"
     )
